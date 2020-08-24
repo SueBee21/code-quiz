bab01a1b2f673dc2d3ca1e62a92efd12300f78fe
SusanBeno <beno.susan@gmail.com>
2020-08-24 07:39:43 -0400

    updating js

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
 // start screen
-var startBtn = document.getElementById("start-button")
-var homeScreen = document.getElementById("home-screen")
-startBtn.addEventListener("click", startQuiz)
+var startBtn = document.getElementById("start-button");
+var homeScreen = document.getElementById("home-screen");
+startBtn.addEventListener("click", startQuiz);
+
+var question = document.getElementById("question");
+var selectChoices = document.getElementById("select-choices");
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
 
 function startQuiz() {
     homeScreen.style.display = "none";
+    var quiz = document.getElementById("quiz");
+    quiz.style.display = "block";
+    askQuestion();
 }
 
+// Questions
 
 
 
+
+let questionList = [
+    {
+        question: "What color is a banana?",
+        choiceA: "A: red",
+        choiceB: "B: purple",
+        choiceC: "C: white",
+        choiceD: "D: yellow",
+        correct: "A",
+    },
+    {
+        question: "This is question 2?",
+        choiceA: "first",
+        choiceB: "second",
+        choiceC: "third",
+        choiceD: "fourth",
+        correct: "C",
+
+    }
+]
+
+var lastQuestion = question.length -1
+var questionNumber = 0
+
+function askQuestion() {
+    var q = questionList[questionNumber];
+
+    question.innerHTML = q.question;
+    choiceA.innerHTML = q.choiceA
+    choiceB.innerHTML = q.choiceB
+    choiceC.innerHTML = q.choiceC
+    choiceD.innerHTML = q.choiceD
+
+}
 // var highscore = document.getElementById("highscore")
 // var time = document.getElementById("time")
 
-// var quiz = document.getElementById("quiz")
-// var question = document.getElementById("question")
-// var selectChoices = document.getElementById("select-choices")
-// var choiceA = document.getElementById("a")
-// var choiceB = document.getElementById("b")
-// var choiceC = document.getElementById("c")
-// var choiceD = document.getElementById("d")
 
 
-// let questions = [
-//     {
-//         question: "What color is a banana?",
-//         choiceA: "red",
-//         choiceB: "purple",
-//         choiceC: "white",
-//         choiceD: "yellow",
-//         correct: "A"
-//     },
-//     {
-//         question: "This is question 2?",
-//         choiceA: "first",
-//         choiceB: "second",
-//         choiceC: "third",
-//         choiceD: "fourth",
-//         correct: "C"
 
-//     }
-// ]
